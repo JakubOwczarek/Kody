31fbe37da6a1e5210351a1415d5574c16bf0dbd0
JakubOwczarek <99959018+JakubOwczarek@users.noreply.github.com>
2022-04-07 19:36:51 +0200

    Add files via upload

---
+++ NWD/NWD.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+
+int NWD(int a, int b)
+{
+    while (a != b)
+        if (a > b)
+            a -= b; 
+        else
+            b -= a; 
+    return a; 
+}
+
+int main()
+{
+    int a, b;
+
+    cout << "Podaj dwie liczby: ";
+    cin >> a >> b;
+
+    cout << "NWD(" << a << "," << b << ") = " << NWD(a, b) << endl;
+
+    return 0;
+}
