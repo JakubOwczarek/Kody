e0374a04e5352cbd8f894693c2c82ca58b97b49f
JakubOwczarek <99959018+JakubOwczarek@users.noreply.github.com>
2022-04-07 19:37:25 +0200

    Add files via upload

---
+++ NWW/NWW.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+
+int NWD(int a, int b)
+{
+	int pom;
+
+	while (b != 0)
+	{
+		pom = b;
+		b = a % b;
+		a = pom;
+	}
+	return a;
+}
+
+int main()
+{
+	unsigned int a, b;
+
+	cout << "Podaj dwie liczby: ";
+	cin >> a >> b;
+
+	cout << "NWW(" << a << ", " << b << ") = " << a / NWD(a, b) * b << endl;
+
+	cin.ignore();
+	cin.get();
+
+	return 0;
+}
